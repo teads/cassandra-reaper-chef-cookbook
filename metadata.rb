bbe7670a329756fc2a0b49cccd34e5436b72806d
Romain Hardouin <romain_hardouin@yahoo.fr>
2018-06-07 23:57:35 +0200

    Set version manually

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,18 @@
-require 'cookbook-release'
-
 name             'cassandra-reaper'
 maintainer       'Teads'
 maintainer_email 'romain.hardouin@teads.tv'
 license          'Apache-2.0'
 description      'Installs Cassandra Reaper, a repair service for Apache Cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          Release.current_version(__FILE__)
+version          '0.1.1'
 chef_version     '>= 12'
 
 source_url 'https://github.com/teads/cassandra-reaper-chef-cookbook'
 issues_url 'https://github.com/teads/cassandra-reaper-chef-cookbook/issues'
 
-supports 'debian'
-supports 'ubuntu'
-supports 'centos'
+%w(ubuntu debian redhat centos).each do |os|
+  supports os
+end
 
 depends 'java'
 depends 'tar'
