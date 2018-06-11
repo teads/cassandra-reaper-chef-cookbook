99c9270574d90cd689c6927121a76d1012729d88
Romain Hardouin <romain_hardouin@yahoo.fr>
2018-06-11 15:54:55 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'romain.hardouin@teads.tv'
 license          'Apache-2.0'
 description      'Installs Cassandra Reaper, a repair service for Apache Cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 chef_version     '>= 12'
 
 source_url 'https://github.com/teads/cassandra-reaper-chef-cookbook'
